1f2efcf0e62d87dc6b4f824866ce97686b6ffb3b
tracer-ics2015 <tracer@njuics.org>
2015-08-15 23:08:31 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  23:08:31 up  1:04,  3 users,  load average: 0.44, 0.30, 0.28 57ee4c1d2a2dd9c55a2d970e7a7eae69c1ed35c9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -35,14 +35,14 @@ static int cmd_c(char *args) {
 
 
 static int cmd_info(char *args) {
-	printf("EAX = [%02X %02X %02X %02X]\n",cpu.gpr[0]._byte[3],cpu.gpr[0]._byte[2],cpu.gpr[0]._byte[1],cpu.gpr[0]._byte[0]);
-	printf("ECX = [%02X %02X %02X %02X]\n",cpu.gpr[1]._byte[3],cpu.gpr[1]._byte[2],cpu.gpr[1]._byte[1],cpu.gpr[1]._byte[0]);
-	printf("EDX = [%02X %02X %02X %02X]\n",cpu.gpr[2]._byte[3],cpu.gpr[2]._byte[2],cpu.gpr[2]._byte[1],cpu.gpr[2]._byte[0]);
-	printf("EBX = [%02X %02X %02X %02X]\n",cpu.gpr[3]._byte[3],cpu.gpr[3]._byte[2],cpu.gpr[3]._byte[1],cpu.gpr[3]._byte[0]);
-	printf("ESP = [%02X %02X %02X %02X]\n",cpu.gpr[4]._byte[3],cpu.gpr[4]._byte[2],cpu.gpr[4]._byte[1],cpu.gpr[4]._byte[0]);
-	printf("EBP = [%02X %02X %02X %02X]\n",cpu.gpr[5]._byte[3],cpu.gpr[5]._byte[2],cpu.gpr[5]._byte[1],cpu.gpr[5]._byte[0]);
-	printf("ESI = [%02X %02X %02X %02X]\n",cpu.gpr[6]._byte[3],cpu.gpr[6]._byte[2],cpu.gpr[6]._byte[1],cpu.gpr[6]._byte[0]);
-	printf("EDI = [%02X %02X %02X %02X]\n",cpu.gpr[7]._byte[3],cpu.gpr[7]._byte[2],cpu.gpr[7]._byte[1],cpu.gpr[7]._byte[0]);
+	printf("EAX = [ %02X %02X %02X %02X ]\n",cpu.gpr[0]._byte[3],cpu.gpr[0]._byte[2],cpu.gpr[0]._byte[1],cpu.gpr[0]._byte[0]);
+	printf("ECX = [ %02X %02X %02X %02X ]\n",cpu.gpr[1]._byte[3],cpu.gpr[1]._byte[2],cpu.gpr[1]._byte[1],cpu.gpr[1]._byte[0]);
+	printf("EDX = [ %02X %02X %02X %02X ]\n",cpu.gpr[2]._byte[3],cpu.gpr[2]._byte[2],cpu.gpr[2]._byte[1],cpu.gpr[2]._byte[0]);
+	printf("EBX = [ %02X %02X %02X %02X ]\n",cpu.gpr[3]._byte[3],cpu.gpr[3]._byte[2],cpu.gpr[3]._byte[1],cpu.gpr[3]._byte[0]);
+	printf("ESP = [ %02X %02X %02X %02X ]\n",cpu.gpr[4]._byte[3],cpu.gpr[4]._byte[2],cpu.gpr[4]._byte[1],cpu.gpr[4]._byte[0]);
+	printf("EBP = [ %02X %02X %02X %02X ]\n",cpu.gpr[5]._byte[3],cpu.gpr[5]._byte[2],cpu.gpr[5]._byte[1],cpu.gpr[5]._byte[0]);
+	printf("ESI = [ %02X %02X %02X %02X ]\n",cpu.gpr[6]._byte[3],cpu.gpr[6]._byte[2],cpu.gpr[6]._byte[1],cpu.gpr[6]._byte[0]);
+	printf("EDI = [ %02X %02X %02X %02X ]\n",cpu.gpr[7]._byte[3],cpu.gpr[7]._byte[2],cpu.gpr[7]._byte[1],cpu.gpr[7]._byte[0]);
 
 	printf("EAX=%08X\n",cpu.eax);
 	printf("ECX=%08X\n",cpu.ecx);
