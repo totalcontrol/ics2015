3b8c438968e22225e59155e0ad1604070d3a724e
tracer-ics2015 <tracer@njuics.org>
2015-08-17 09:22:31 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  09:22:31 up  6:51,  3 users,  load average: 0.03, 0.22, 0.28 cc099f84d061c60acfedfb9bff244b7df3ef175c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -136,7 +136,7 @@ int eval(int p,int q)
     else {
         int i,j;op=p;
 		for (i=2;i<NR_REGEX;i++)
-		  for (j=p;j<p;j++)
+		  for (j=p;j<q;j++)
 		  	if (rules[i].token_type==tokens[j].type)
 		  		{
 		  		  op=j;
