3091ff7273c00948b1c815651ac624ff4ce885a1
tracer-ics2015 <tracer@njuics.org>
2015-08-17 16:37:34 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  16:37:34 up 12:34,  3 users,  load average: 0.40, 0.31, 0.27 97ba9314407dcf891a8bb83ce6092ecec6406b46

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -233,7 +233,7 @@ int eval(int p,int q)
             case '+': return val1 + val2;
             case '-': return val1 - val2;/* ... */
             case '*': return val1 * val2;/* ... */
-            case '/': /* ... */
+            case EQ:  return val1==val2; /* ... */
             default: assert(0);
 			return 0;
         }
