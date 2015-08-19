e7d3bafd50b3525b006c89dcee6a9ac76f6878be
tracer-ics2015 <tracer@njuics.org>
2015-08-19 08:55:39 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  08:55:39 up  1:42,  3 users,  load average: 0.17, 0.17, 0.21 73504842ab386ab4c6486961af1cd62d9db866

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -86,10 +86,10 @@ static int cmd_display_mem(char *args) {
 
 
 static int cmd_print_exp(char *args) {
-int number=0;bool bb;
+    int number=0;bool bb;
 	number = expr(args,&bb);
-  printf("___          %s = %8x          ____\n",args,number);
-  return 0;
+    printf("      [%s]=[%08x]\n",args,number);
+    return 0;
 
 }
 
