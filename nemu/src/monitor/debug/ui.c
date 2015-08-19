d317a81b46f6c37cf1f656237c9d78bf2fa419d5
tracer-ics2015 <tracer@njuics.org>
2015-08-19 10:51:00 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  10:51:00 up  2:07,  3 users,  load average: 0.29, 0.37, 0.35 abd5ed8ad6af12878ddd1e635d36805ef3f11fe5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -70,7 +70,7 @@ static int cmd_print_mem(char *args) {
 			 for(i=0;i<number;i++)
 			 	{
 			 	
-		 	     printf("        Mem[%d]: [%08X]\n",number,swaddr_read(value+i,4));
+		 	     printf("        Mem[%d]: [%08X]\n",value+i,swaddr_read(value+i,4));
 			 	}
 			 }
 		}
