b5c3835c5e2a841ab491057a3309edb6d533f6cd
tracer-ics2015 <tracer@njuics.org>
2015-08-27 13:53:04 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  13:53:04 up  3:45,  3 users,  load average: 0.58, 0.32, 0.31 13b8a7d0d0eda6cb24e439cc89f1acc3dedb59d4

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -8,7 +8,8 @@ static void do_execute () {
 	src = op_src->val;
 	result= dest-src;
     result=(DATA_TYPE)0x0; 
-	cpu.ZF=(result==0)?1:0; 
+	cpu.ZF=(result==0)?1:0;   //test ok
+	printf("DATA_TYPE__%d\n",sizeof(DATA_TYPE));
 	cpu.SF=((result>>(sizeof(DATA_TYPE)*8-1))==0)?0:1;
 	cpu.OL=1;
 	
