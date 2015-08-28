3e7741d4bc7e5a675ae78919f41051ef543e709a
tracer-ics2015 <tracer@njuics.org>
2015-08-28 21:39:24 +0800

    >  compile NEMU 141220000 root Linux stan-VirtualBox 3.13.0-24-generic #46-Ubuntu SMP Thu Apr 10 19:08:14 UTC 2014 i686 i686 i686 GNU/Linux  21:39:24 up 13:20,  3 users,  load average: 0.33, 0.39, 0.40 1a1454ef6e3941a419a21b5d7d3e8aaae38d77e

--- nemu/src/cpu/exec/arith/test-template.h
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -5,36 +5,32 @@
 
 
 static void do_execute () {
-	DATA_TYPE result,dest,src,mask8,mask4;
+	DATA_TYPE result,dest,src,mask8;
 	uint8_t lsb8bits;
-    uint8_t srcMsb,destMsb,resultMsb;
+    uint8_t resultMsb;
 
 	mask8= (DATA_TYPE)0x00000FF;
-	mask4= (DATA_TYPE)0x000000F;
 	
 	dest= op_dest->val;   //val is always 32bits, so we must change format;
 	src = op_src->val;
-	result= dest-src;
+	result= dest&src;
 
-	printf("dest=%x  src=%x  result=%x\n",dest,src,result);
+	printf("test dest=%x  src=%x  result=%x\n",dest,src,result);
 	
-	OPERAND_W(op_dest, result);
 	
 	/* TODO: Update EFLAGS. */
 		//panic("please implement me");
 
 	lsb8bits=(uint8_t)(result&mask8);
 	//compute signed bits
-	srcMsb=((src>>(sizeof(DATA_TYPE)*8-1))==0)?0:1;
-	destMsb=((dest>>(sizeof(DATA_TYPE)*8-1))==0)?0:1;	
+
 	resultMsb=((result>>(sizeof(DATA_TYPE)*8-1))==0)?0:1;
 
-    cpu.OF=(((~destMsb&srcMsb&resultMsb)|(destMsb&~srcMsb&~resultMsb))==1)?1:0;
-	cpu.CF=(dest<src)?1:0;
+    cpu.OF=0;
+	cpu.CF=0;
 	cpu.ZF=(result==0)?1:0;   //test ok
 	//printf("DATA_TYPE__%d\n",sizeof(DATA_TYPE));
 	cpu.SF=(resultMsb==1)?1:0; //test ok
-    cpu.AF=(((dest&mask4)-(src&mask4))>mask4)?1:0;  // dec overflow, bcd compute	cpu.PF=()
     cpu.PF=(true==checkevenparity(lsb8bits))?1:0;  //test ok
 	//assert(cpu.AF==0);
 
